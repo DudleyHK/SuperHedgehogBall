7d86a8c0af6a7e3165a493381fdb27aa8f163189
g2-pelns <Gundars2.Pelns@live.uwe.ac.uk>
2017-09-27 12:39:14 +0100

    Added Ball in Main
    it falls

---
+++ Assets/Objects/Player/BallFall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 ballPos = this.transform.position;
+        ballPos.y -= 0.05f;
+        this.transform.position = ballPos;
+	}
+}

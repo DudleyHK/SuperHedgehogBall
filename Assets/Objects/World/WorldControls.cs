46bb1eb394b33da2b665e983a705bc5283d38e0e
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-09-27 13:03:23 +0100

    worldcontrol script added

---
+++ Assets/Objects/World/WorldControls.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldControls : MonoBehaviour
+{
+    // Get origin of player
+    // Rotate world object around player origin 
+}

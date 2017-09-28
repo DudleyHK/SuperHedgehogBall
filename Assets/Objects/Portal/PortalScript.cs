02726c51054e0eaa31ac15b1ca101da81de2dbe8
Gundars <Gundars2.Pelns@live.uwe.ac.uk>
2017-09-28 18:02:17 +0100

    Portal Work
    Code is broken

--- Assets/Objects/Portal/PortalScript.cs
+++ Assets/Objects/Portal/PortalScript.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class PortalScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject player;
+    public GameObject direction;
+    public GameObject spawn;
+
+    private float speed = 5.0f;
+
+    private Vector2 dirPos;
+    private Vector2 spaPos;
+    private Vector2 plaPos;
+
+    // Use this for initialization
+    void Start () {
+        plaPos = player.transform.postition;
+        spaPos = spawn.transform.position;
+        dirPos = direction.transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        plaPos = Vector2.MoveTowards(spaPos, dirPos, speed * Time.deltaTime);
+    }
 }

d514b141196e1a220e23b051f1002d4f21502015
g2-pelns <Gundars2.Pelns@live.uwe.ac.uk>
2017-09-29 11:58:41 +0100

    Portals.. God Damn
    Triggers are being a lil annyoing

--- Assets/Objects/Portal/PortalScript.cs
+++ Assets/Objects/Portal/PortalScript.cs
@@ -10,24 +10,40 @@ public class PortalScript : MonoBehaviour {
 
     private float speed = 5.0f;
 
+    public bool sw1tch;
+
     private Vector2 dirPos;
     private Vector2 spaPos;
     private Vector2 plaPos;
 
+    private PortalScript script;
+
     // Use this for initialization
     void Start () {
-        plaPos = player.transform.postition;
-        spaPos = spawn.transform.position;
-        dirPos = direction.transform.position;
+        sw1tch = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+    }
 
-    void OnTriggerEnter2D(Collider2D other)
+    IEnumerator OnTriggerEnter2D(Collider2D other)
     {
-        plaPos = Vector2.MoveTowards(spaPos, dirPos, speed * Time.deltaTime);
+        if (sw1tch == true)
+        {
+            GetComponentInChildren<BoxCollider2D>().enabled = false;
+            yield return new WaitForSeconds(2);
+            player.transform.position = spawn.transform.position;
+
+            sw1tch = false;
+            yield return new WaitForSeconds(6);
+            sw1tch = true;
+        }
+        //will think of better timer later
+    }
+
+    void Teleport()
+    {
+
     }
 }

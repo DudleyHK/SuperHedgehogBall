2576d2074d6be2feb983787d234bfc93d5ff69a4
Alexander Pagliarini <Alexander2.Pagliarini@live.uwe.ac.uk>
2017-09-30 14:18:06 +0100

    Better camera

--- Assets/Objects/Camera/CameraMovement.cs
+++ Assets/Objects/Camera/CameraMovement.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public GameObject player;
+    public float acceleration; //float between 0 and 1
+    private Vector3 velocity;
 
-
-	private void Update ()
+    private void Start()
     {
-        var playerPos = player.transform.position;
-        var newCamPos = new Vector3(playerPos.x, playerPos.y, this.transform.position.z);
-        transform.position = newCamPos;
+
+    }
+
+    private void Update()
+    {
+        //Legacy Camera Movement
+        //var playerPos = player.transform.position;
+        //var newCamPos = new Vector3(playerPos.x, playerPos.y, this.transform.position.z);
+        //transform.position = newCamPos;
+
+        //Juicy Camera Movement
+        velocity = (player.transform.position - this.transform.position) * acceleration;
+        this.transform.Translate(velocity.x, velocity.y, 0);
+
     }
 }

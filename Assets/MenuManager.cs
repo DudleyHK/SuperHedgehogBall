68d544533f7325fc7477cf0a31b3c2f5355b3e89
freakishhero <mitchell.height@gmail.com>
2017-10-01 12:24:48 +0100

    Added a level selection menu from the main menu where you can currently select one of three debug levels we have so far. Refactored the timer so that it's not complete garbage... Also miliseconds are now displayed.

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -3,9 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void StartGame()
+    public void LoadLevel1()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadLevelSelector()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(4);
     }
 
     public void QuitGame()

fe71eb0a61149515ce7129c5865b2d24110efbc2
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-10-01 16:45:05 +0100

    level selection stuff

---
+++ Assets/Objects/Menu/LevelSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+    public Button levelOne;
+    public Button levelTwo;
+    public Button levelThree;
+
+    private void Start()
+    {
+        levelOne.onClick.AddListener(() => LevelManager.LoadScene("LevelOne"));
+        levelTwo.onClick.AddListener(() => LevelManager.LoadScene("LevelTwo"));
+        levelThree.onClick.AddListener(() => LevelManager.LoadScene("LevelThree"));
+    }
+
+
+
+}

0aeab4bc74e18b5a5d7e991900ee68ddff57105a
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-10-03 18:56:29 +0100

    camera shake

--- Assets/Objects/Camera/CameraShake.cs
+++ Assets/Objects/Camera/CameraShake.cs
@@ -6,20 +6,15 @@ public class CameraShake : MonoBehaviour
 {
     public float shakeTimer = 0f;
     public float shakeAmount = 0f;
+    
 
 
-	void Start ()
-    {
-		
-	}
-	
 	void Update ()
     {
 	    if(shakeTimer >= 0f)
         {
             Vector2 shakePos = Random.insideUnitCircle * shakeAmount;
             transform.position = new Vector3(transform.position.x + shakePos.x, transform.position.y + shakePos.y, transform.position.z);
-
 
             shakeTimer -= Time.deltaTime;
         }
